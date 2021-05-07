5758aa1646b1bd577f016233cea74ff9729fdcf3
PijiuLaoshi <pijiulaoshi@gmail.com>
2021-05-07 18:01:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pygreensens-pjlsh",
+    name="pygreensens",
     version="0.1",
     author="PijiuLaoshi",
     author_email="pijiulaoshi@gmail.com",
