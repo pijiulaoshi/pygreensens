7bbd233fef974237109ac7d7157661c22b8f6260
PijiuLaoshi <pijiulaoshi@gmail.com>
2021-06-10 19:06:15 +0200

    fixed http errors (hopefully)

--- src/pygreensens/api.py
+++ src/pygreensens/api.py
@@ -1,9 +1,18 @@
 import requests
+from requests.adapters import HTTPAdapter
 import urllib3
+from requests.packages.urllib3.util.retry import Retry
 
-urllib3.disable_warnings()
+
 import json
 from datetime import date
+import logging
+_LOGGER = logging.getLogger('pygreensens')
+
+forcelist=[413, 429, 500, 502, 503, 504]
+backoff = 1
+retries = 3
+adapt_set = Retry(total=retries, backoff_factor=backoff, status_forcelist=forcelist)
 
 
 class GreensensApi:
@@ -11,13 +20,17 @@ class GreensensApi:
         self._user = username
         self._pass = password
         self._host = "https://api.greensens.de/api"
+        self._headers = {"Content-Type": "application/json"}
+        self._adapt = HTTPAdapter(max_retries=adapt_set)
+        urllib3.disable_warnings()
         self.s = requests.Session()
+        self.s.mount("https://", self._adapt)
         self._at = None
         self._atd = None
+        
         self.authenticate()
-
         self._bearer = f"Bearer {self._at}"
-        self._headers = {"Content-Type": "application/json"}
+        
         self._data = None
         # self._hubs = None
         self._sensors = None
@@ -37,7 +50,7 @@ class GreensensApi:
         return self._sensors
 
     def update_sensors(self):
-        """Return sensor list"""
+        """Update sensor list"""
         list = []
         for key, value in self._data.items():
             list.append(key)
@@ -53,26 +66,25 @@ class GreensensApi:
         self.update_access_token()
         headers = self._headers
         headers["authorization"] = self._bearer
-        data = self.s.get(
-            f"{self._host}/plants", headers=headers, verify=False, timeout=10
-        )
-        if data.status_code == 200:
-            hubs = data.json()["data"]["registeredHubs"]
-            new_data = {}
-            for hub in hubs:
-                for sensor in hub["plants"]:
-                    new_data[sensor["sensorID"]] = sensor
-            return new_data
-        else:
-            return self._data
-
+        try:
+            r = self.s.get(f"{self._host}/plants", headers=headers, verify=False)
+            if r.status_code == 200:
+                hubs = r.json()["data"]["registeredHubs"]
+                new_data = {}
+                for hub in hubs:
+                    for sensor in hub["plants"]:
+                        new_data[sensor["sensorID"]] = sensor
+                return new_data
+            else:
+                return self._data
+        except:
+            _LOGGER.error('Could not connect to GreenSens cloud')
+            
     ## AUTH ##
     def authenticate(self):
         url = f"{self._host}/users/authenticate"
         payload = json.dumps({"login": self._user, "password": self._pass})
-        r = self.s.post(
-            url, headers={"Content-Type": "application/json"}, data=payload, timeout=10
-        )
+        r = self.s.post(url, headers=self._headers, data=payload)
         token = r.json()["data"]["token"]
         auth_date = date.today()
         self._at = token
